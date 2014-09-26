11a96ccb94cbb5b2f0773d0625e2bc1a83924af6
Thomas W <thomas@matchstrike.net>
2014-09-26 15:38:10 -0700

    Setup project directory and install testing requirements

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+from djplatter import __version__, __author__
+
+#TODO: Description, classifiers
+setup(
+    name="djplatter",
+    version=__version__,
+    url='https://github.com/thomasw/djplatter',
+    download_url='https://github.com/thomasw/djplatter/releases',
+    author=__author__,
+    author_email='thomas.welfley+djplatter@gmail.com',
+    description='',
+    packages=find_packages(),
+    tests_require=[
+        'mock==1.0.1', 'nose==1.3.4', 'pinocchio==0.4.1', 'unittest2==0.5.1',
+        'django==1.7'],
+    install_requires=['django>=1.4'],
+    classifiers=[],
+    test_suite='nose.collector'
+)
